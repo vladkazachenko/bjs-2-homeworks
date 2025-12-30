ece97e4d70893484bc5dcbaf38b266876e95faa9
Vlad Kazachenko <vladkazachenko95@gmail.com>
2025-12-30 16:56:36 +0300

    Решение задач лекции 3#2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,18 +1,24 @@
 function compareArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
   return arr1.every((item, index) => item === arr2[index]);
 }
 function getUsersNamesInAgeRange(users, gender) {
-  if (users.length === 0) return 0;
+  if (users.length === 0) {
+    return 0;
+  }
 
-  const ages = users
-    .filter(user => user.gender === gender)
-    .map(user => user.age);
+  const filteredUsers = users.filter(user => user.gender === gender);
 
-  if (ages.length === 0) return 0;
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
 
-  const sum = ages.reduce((acc, age) => acc + age, 0);
+  const sumAges = filteredUsers.reduce((sum, user) => {
+    return sum + user.age;
+  }, 0);
 
-  return sum / ages.length;
+  return sumAges / filteredUsers.length;
 }

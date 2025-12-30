ff78939efdaea5f7448c15bfd5d4c516460e6d6b
Vlad Kazachenko <vladkazachenko95@gmail.com>
2025-12-30 17:31:08 +0300

    Решение задач лекции 6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount(value) {
+  const result = Number.parseFloat(value);
+  if (Number.isNaN(result)) {
+    throw new Error("Невалидное значение");
+  }
+  return result;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+class Triangle {
+  constructor(a, b, c) {
+    if (
+      a + b <= c ||
+      a + c <= b ||
+      b + c <= a
+    ) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    const p = this.perimeter / 2;
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    return Number(area.toFixed(3));
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      }
+    };
+  }
+}

9a59c46463c241e843d42a85a1b0d8b02b7398c1
Vlad Kazachenko <vladkazachenko95@gmail.com>
2026-01-10 11:47:51 +0300

    Решение задач лекции 7

---
+++ 7.async/task.js
@@ -0,0 +1,69 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) {
+      throw new Error('Отсутствуют обязательные аргументы');
+    }
+
+    const isExist = this.alarmCollection.some(alarm => alarm.time === time);
+    if (isExist) {
+      console.warn('Уже присутствует звонок на это же время');
+    }
+
+    this.alarmCollection.push({
+      time,
+      callback,
+      canCall: true,
+    });
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(
+      alarm => alarm.time !== time
+    );
+  }
+
+  getCurrentFormattedTime() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
+
+  start() {
+    if (this.intervalId !== null) {
+      return;
+    }
+
+    this.intervalId = setInterval(() => {
+      const currentTime = this.getCurrentFormattedTime();
+
+      this.alarmCollection.forEach(alarm => {
+        if (alarm.time === currentTime && alarm.canCall) {
+          alarm.canCall = false;
+          alarm.callback();
+        }
+      });
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach(alarm => {
+      alarm.canCall = true;
+    });
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
